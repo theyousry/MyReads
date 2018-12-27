3bdc8feef6202af9f69d713317a98a836531bf0c
theyousry <theyousry@gmail.com>
2018-12-27 13:27:40 +0200

    Updating bookshelves from mainpage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 import { Route } from 'react-router-dom'
 import Search from './Search'
@@ -7,11 +7,39 @@ import Main from './Main'
 
 class BooksApp extends Component {
 
+  state = {
+    books: []
+}
+
+    async componentDidMount() {
+        const books = await BooksAPI.getAll()
+        this.setState( { books } )
+              console.log('currently reading: ', books.filter(book => book.shelf === "currentlyReading"))
+              console.log('want to read: ', books.filter(book => book.shelf === "wantToRead"))
+              console.log('read: ', books.filter(book => book.shelf === "read"))
+    }
+
+    updateShelf = (book, shelf) => {
+       BooksAPI.update(book, shelf)
+        .then((response) => {
+          book.shelf = shelf;
+          this.setState((state) => ({
+            books: state.books.filter((x) => {
+              return (x.id !== book.id)
+            }).concat([book])
+          }))
+        })
+      }
   render() {
     return (
       <div className="app">
-      <Route exact path='/' component ={Main} />
-      <Route path='/search' render ={() => (
+      <Route exact path='/' render={() => (
+        <Main
+          books={this.state.books}
+          updateShelf={this.updateShelf}
+        />
+      )} />
+      <Route path='/search' render={() => (
         <Search />
       )} />
       </div>
--- src/Shelf.js
+++ src/Shelf.js
@@ -3,30 +3,23 @@ import Book from './Book'
 
 class Shelf extends Component {
     render() {
+      const { books, updateShelf } = this.props
         return (
         <div className="bookshelf">
-            <h2 className="bookshelf-title">{this.props.title}</h2>
+            <h2 className="bookshelf-title">{this.props.section}</h2>
             <div className="bookshelf-books">
                 <ol className="books-grid">
-                <Book />
-                {/*<li>
-                    <div className="book">
-                    <div className="book-top">
-                        <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")' }}></div>
-                        <div className="book-shelf-changer">
-                        <select>
-                            <option value="move" disabled>Move to...</option>
-                            <option value="currentlyReading">Currently Reading</option>
-                            <option value="wantToRead">Want to Read</option>
-                            <option value="read">Read</option>
-                            <option value="none">None</option>
-                        </select>
-                        </div>
-                    </div>
-                    <div className="book-title">Ender's Game</div>
-                    <div className="book-authors">Orson Scott Card</div>
-                    </div>
-                </li>*/}
+                {
+                    books && books.map((book) => {
+                         return (
+                              <Book
+                                   book={book}
+                                   key={book.id}
+                                   updateShelf={updateShelf}
+                              />
+                         )
+                    })
+               }
                 </ol>
             </div>
         </div>
