462a270abe07170c9616914a30a31aafa09cd41b
theyousry <theyousry@gmail.com>
2018-12-27 12:36:46 +0200

    components Refactored & react-router Implemented

---
+++ build/precache-manifest.3d8e8fe90bdf0a1f6a31d0308ea78fe7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a1e5121f7bf5c4a9fb15",
+    "url": "/static/js/main.a1e5121f.chunk.js"
+  },
+  {
+    "revision": "f68dcc1a0c0c6e129be0",
+    "url": "/static/js/1.f68dcc1a.chunk.js"
+  },
+  {
+    "revision": "a1e5121f7bf5c4a9fb15",
+    "url": "/static/css/main.208e4b7c.chunk.css"
+  },
+  {
+    "revision": "2e2d11a5874fc699451d6e73b3564da0",
+    "url": "/index.html"
+  }
+];
